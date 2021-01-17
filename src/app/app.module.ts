f6ad354a598e14fcd2b750de67afb15e3fda13f5
Wei Ding <ding_web@yahoo.co.jp>
2021-01-17 04:44:45 -0500

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SiteFormComponent } from './site-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    SiteFormComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
