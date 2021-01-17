60162ea4a84280c0c136e8544ff87fa9821b4cbe
Wei Ding <ding_web@yahoo.co.jp>
2021-01-17 12:01:39 -0500

    start working

--- src/app/site-form.component.ts
+++ src/app/site-form.component.ts
@@ -6,10 +6,10 @@ import { Site } from './site';
     selector: 'site-form',
     templateUrl: 'site-form.component.html'
 })
-export class SiteFormComponent{
+export class SiteFormComponent {
     urls = ['www.yahoo.com','www.google.com','www.apple.com','www.facebook.com'];
     model = new Site(1,'Yahoo',this.urls[0], 10000);
     submitted = false;
     onSubmit() { this.submitted = true; }
-    get diagnostic() { return JSON.stringify(this.model); }
+    //get diagnostic() { return JSON.stringify(this.model); }
 }
