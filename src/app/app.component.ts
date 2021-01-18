673414d67dbd9c748ca41aee33b1771e495548ca
Wei Ding <ding_web@yahoo.co.jp>
2021-01-17 20:46:42 -0500

    two routers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h2 style="color: red">Layer 2: Component</h2><site-form>Loading site-form ...</site-form>'
+  template: `
+    <h2 style="color: red">Layer 2: Component</h2>
+    <site-form>Loading site-form ...</site-form>
+    <ul>
+      <li><a [routerLink] = "['/Product']">Product</a></li>
+      <li><a [routerLink] = "['/Inventory']">Inventory</a></li>
+    </ul>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
 }
